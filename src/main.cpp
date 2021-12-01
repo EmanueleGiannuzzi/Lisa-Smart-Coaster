9f4672a413f00c830c33da0922f3e7a2bc323b87
Jan <8350867+EmanueleGiannuzzi@users.noreply.github.com>
2021-12-01 14:32:40 +0100

    Neopixels

--- src/main.cpp
+++ src/main.cpp
@@ -2,38 +2,34 @@
 
 #include <EEPROM.h>
 #include <Ticker.h>
-#include <RGBLed.h>
 #include <avdweb_Switch.h>
+#include <Adafruit_NeoPixel.h>
 
-#define EEPROM_ADDRESS 0
-
-#define RED_PIN 11
-#define GREEN_PIN 10
-#define BLUE_PIN 9
+#define TIME_DIVIDER 60 //Debug purpose
 
 #define BUTTON_PIN 6
+#define PIXELS_PIN 3
+#define NUMPIXELS 8
 
-#define FLASH_RATE 500 //on/off each FLASH_RATE millisecond
-
-#define TIME_DIVIDER 1 //Debug purpose
-#define STEP_PER_SEC 1
-
-#define COLOR_UPDATE_FREQ 50 //millis
-
-#define DEFAULT_TIMER_SETTING 0
-
-#define CUP_UP_DELAY 1000
-
+#define BRIGHTNESS 150 //0-255
 #define TURN_OFF_DELAY 120 // seconds
 
+#define DEFAULT_TIMER_SETTING 0
+#define CUP_UP_DELAY 1000
 const int DEFAULT_OFF_COLOR[3] = {0, 0, 0};
+
+#define EEPROM_ADDRESS 0
+#define FLASH_RATE 500 //on/off each FLASH_RATE millisecond
+#define STEP_PER_SEC 1
+#define COLOR_UPDATE_FREQ 50 //millis
+
 
 const int timerSettingsSize = 6;
 int currentTimerSetting = DEFAULT_TIMER_SETTING;
 int timerSettings[] = {15, 30, 45, 60, 90, 120};
 
-RGBLed led(RED_PIN, GREEN_PIN, BLUE_PIN, RGBLed::COMMON_CATHODE);
 Switch button = Switch(BUTTON_PIN);
+Adafruit_NeoPixel pixels(NUMPIXELS, PIXELS_PIN, NEO_GRB + NEO_KHZ800);
 
 #pragma region FlashTicker
 
@@ -49,13 +45,33 @@ bool* onOffPattern;
 unsigned int patternSize = 0;
 unsigned int currentOnOff = 0;
 
+
+
+void setAllColor(uint8_t r, uint8_t g, uint8_t b) {
+  for(int i=0; i<NUMPIXELS; i++) {
+    pixels.setPixelColor(i, pixels.Color(r, g, b));
+  }
+  pixels.show();
+}
+
+void startAnimation() {
+
+  for(int i=0; i<NUMPIXELS; i++) {
+    pixels.setPixelColor(i, pixels.Color(0, 255, 0));
+    pixels.show(); 
+    delay(500);
+  }
+
+  setAllColor(DEFAULT_OFF_COLOR[0], DEFAULT_OFF_COLOR[1], DEFAULT_OFF_COLOR[2]);
+}
+
 void flashTick(){
   bool flashOn = onOffPattern[currentOnOff];
   if(flashOn) {
-    led.setColor(flashOnRed, flashOnGreen, flashOnBlue);
+    setAllColor(flashOnRed, flashOnGreen, flashOnBlue);
   }
   else {
-    led.setColor(flashOffRed, flashOffGreen, flashOffBlue);
+    setAllColor(flashOffRed, flashOffGreen, flashOffBlue);
   }
   currentOnOff++;
   if(currentOnOff >= patternSize) {
@@ -91,13 +107,13 @@ void startFlash(bool* pattern, int _patternSize, const int onColor[3], const int
 
 #pragma endregion
 
-
 bool isCupDown = false;
 bool isInConfigMode = false;
 
 int colorStage = 0;//0 = blue; 1 = blue->purple; 2 = purple->red; 3 = red; 4 = red flashing
 float currentStageProgress = 0;
 int steps = 0;
+
 
 void ledLoop() {
   static int red = 0;
@@ -107,26 +123,36 @@ void ledLoop() {
   static unsigned long lastMillis = millis();
   static bool flashOn = false;
 
+  float weight = 1.0f;
+
   switch (colorStage) {
     case 0://blue
       blue = 255;
       green = 0;
       red = 0;
+
+      // weight = 0.5;
       break;
     case 1://blue->purple
       blue = 255;
       green = 0;
       red = currentStageProgress * 255;
+
+      // weight = 2.0f;
       break;
     case 2://purple->red
       blue =  255 - (currentStageProgress * 255);
       green = 0;
       red = 255;
+
+      // weight = 2.0f;
       break;
     case 3://red
       blue = 0;
       green = 0;
       red = 255;
+      
+      // weight = 0.5f;
       break;
     case 4://red flashing
       blue = 0;
@@ -141,7 +167,7 @@ void ledLoop() {
       break;
   }
 
-  currentStageProgress += 1.0f / steps;
+  currentStageProgress += (1.0f / steps) / weight;
   
   if(currentStageProgress >= 1.0f) {
     if(colorStage < 4) {
@@ -150,7 +176,7 @@ void ledLoop() {
     currentStageProgress = 0.0f;
   }
 
-  led.setColor(red, green, blue);
+  setAllColor(red, green, blue);
 }
 
 Ticker ledTicker(ledLoop, COLOR_UPDATE_FREQ, 0, MILLIS);
@@ -158,7 +184,8 @@ Ticker ledTicker(ledLoop, COLOR_UPDATE_FREQ, 0, MILLIS);
 void turnOff() {
   Serial.println("OFF");
   ledTicker.stop();
-  led.off();
+  setAllColor(0, 0, 0);
+  pixels.clear();
 }
 Ticker* turnOffTicker;
 
@@ -182,13 +209,13 @@ void onCupUp() {
   isCupDown = false;
 
   ledTicker.stop();
-  led.setColor(DEFAULT_OFF_COLOR[0], DEFAULT_OFF_COLOR[1], DEFAULT_OFF_COLOR[2]);
+  setAllColor(DEFAULT_OFF_COLOR[0], DEFAULT_OFF_COLOR[1], DEFAULT_OFF_COLOR[2]);
 }
 
 Ticker cupUpTicker(onCupUp, CUP_UP_DELAY, 1, MILLIS);
 
 void onCupDown() {
-  led.setColor(RGBLed::BLUE);
+  setAllColor(0, 0, 255);
   Serial.println("Cup down");
   isCupDown = true;
   cupUpTicker.stop();
@@ -216,7 +243,6 @@ void flashSetting() {
 }
 
 void onEnterConfigMode() {
-
   Serial.print("Config mode ");
   Serial.println(currentTimerSetting + 1);
   isInConfigMode = true;
@@ -227,7 +253,8 @@ void onEnterConfigMode() {
   if(turnOffTicker != nullptr) {
     turnOffTicker->stop();
   }
-  led.off();
+  setAllColor(0, 0, 0);
+  pixels.clear();
 
   flashSetting();
 }
@@ -237,9 +264,9 @@ void onExitConfigMode() {
   isInConfigMode = false;
   stopFlash();
 
-  led.setColor(RGBLed::RED);
+  setAllColor(255, 0, 0);
   delay(400);
-  led.setColor(RGBLed::GREEN);
+  setAllColor(0, 255, 0);
   delay(400);
 
   onCupUp();
@@ -286,7 +313,7 @@ void onSingleClick(void* param) {
 
 
 void setup() {
-  // Serial.begin(9600);
+  Serial.begin(9600);
 
   currentTimerSetting = EEPROM.read(EEPROM_ADDRESS);
   
@@ -295,7 +322,10 @@ void setup() {
   button.setDoubleClickCallback(onDoubleClick);
   button.setSingleClickCallback(onSingleClick);
 
-  led.setColor(DEFAULT_OFF_COLOR[0], DEFAULT_OFF_COLOR[1], DEFAULT_OFF_COLOR[2]);
+  pixels.begin(); 
+  pixels.setBrightness(BRIGHTNESS);
+  pixels.clear();
+  startAnimation();
 }
 
 
